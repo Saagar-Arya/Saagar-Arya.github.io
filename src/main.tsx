bfb7e1eed407fb4a84de860b20ff7622a4a1e004
Saagar Arya <saagar.arya@duke.edu>
2024-08-25 17:49:32 -0400

    saving

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,15 @@ import * as React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const rootElement = document.getElementById('root')!;
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ChakraProvider>
         <App />
       </ChakraProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
